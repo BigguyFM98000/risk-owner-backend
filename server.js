5c55fcf172c63ef5c2bc2d317aa25374772444e0
Fhatuwani Raphalalani <fm.raphalalani29@gmail.com>
2024-02-29 10:52:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const RiskownerRoutes = require('./routes/risk_owner_routes');
 const PORT = process.env.PORT || 8090
 
 const app = express();
-const corsOptions = {origin: ["http://localhost:3000", "https://risk-owners-frontend.vercel.app/", "https://risk-owner-backend.onrender.com", "*"]}
+const corsOptions = {origin: ["http://localhost:3000", "https://risk-owners-frontend.vercel.app/", "https://risk-owner-backend.onrender.com/riskowner", "*"]}
 app.use(cors(corsOptions))
 
 // Enable CORS for all requests
